83bf88044e1178f2e39939fa03bc9a3c5b252783
ginareeena <ginamoffit@gmail.com>
2021-03-22 16:53:35 -0400

    feat(sockets): draft for sending drawings over sockets

--- FrontEnd/src/Socket.js
+++ FrontEnd/src/Socket.js
@@ -1,23 +1,70 @@
-import React, { useState, useEffect } from "react"
-import socketIOClient from "socket.io-client"
-const ENDPOINT = "http://127.0.0.1:4001"
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://127.0.0.1:4001";
 
 const SocketComp = () => {
-    const [response, setResponse] = useState("")
+  //   const [response, setResponse] = useState("");
+  // by default nobody's drawing
+  const [drawing, sendDrawing] = useState("");
+  //   const socket = socketIOClient(ENDPOINT);
 
-    useEffect(() => {
-        const socket = socketIOClient(ENDPOINT)
-        socket.on("FromAPI", data => {
-            setResponse(data)
-        })
-        return () => socket.disconnect()
-    }, [])
+  //   useEffectOLD(() => {
+  //     const socket = socketIOClient(ENDPOINT);
+  //     socket.on("FromAPI", (data) => {
+  //       setResponse(data);
+  //     });
+  //     return () => socket.disconnect();
+  //   }, []);
 
-    return (
-        <div>
-            <p>It's <time dateTime={response}>{response}</time></p>
-        </div>
-    )
-}
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("drawing", (data) => {
+      sendDrawing(data);
+    });
+    return () => socket.disconnect();
+  }, []);
 
-export default SocketComp
+  let data = { drawing: "drawing" };
+
+  //   socket.emit(
+  //     "data",
+  //     { data: "drawing" }
+  // (data = { drawing: "lines" })
+  // console.log("are you drawing?")
+  //   {
+  //     x0: x0 / w,
+  //     y0: y0 / h,
+  //     x1: x1 / w,
+  //     y1: y1 / h,
+  //     color: color,
+  //   }
+  //   );
+
+  //   let canvasEl = document.getElementById("canvas");
+  let drawingColorEl = document.getElementById("drawing-color");
+  //   let drawingModeEl = document.getElementById("drawing-mode-selector");
+  //   let drawingLineWidthEl = document.getElementById("drawing-line-width");
+  //   let brushSizeText = document.getElementById("brushSize");
+
+  //   canvasEl.addEventListener("mousedown", onMouseDown, false);
+  //   canvasEl.addEventListener("mouseup", onMouseUp, false);
+  //   canvasEl.addEventListener("mouseout", onMouseUp, false);
+  //   canvasEl.addEventListener("mousemove", onMouseMove, false);
+
+  //   function onMouseDown(e) {
+  //     drawing = true;
+
+  //     let x = e.clientX || e.touches[0].clientX;
+  //     let y = e.clientY || e.touches[0].clientY;
+  //   }
+
+  return (
+    <div>
+      <p>
+        It's <time dateTime={response}>{response}</time>
+      </p>
+    </div>
+  );
+};
+
+export default SocketComp;
--- Server/index.js
+++ Server/index.js
@@ -1,48 +1,76 @@
 // const path = require('path')
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 // const morgan = require('morgan')
-const http = require('http')
+const http = require("http");
 
-const index = require('./api/index')
+const index = require("./api/index");
 
-const port = process.env.PORT || 4001
+const port = process.env.PORT || 4001;
 
 // app.use(morgan('dev'))
 
 // app.use(express.json())
 // app.use(express.urlencoded({extended: true}))
 
+app.use(index);
 
-app.use(index)
-
-const server = http.createServer(app)
+const server = http.createServer(app);
 const socketIo = require("socket.io")(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
-})
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
-let interval
+// sending time
+
+let interval;
+
+const getApiAndEmit = (socket) => {
+  const response = new Date();
+  socket.emit("FromAPI", response);
+};
+
+// drawing
+
+//listening for drawings maybe
+let getDrawingAndEmit = (socket) => {
+  //   const drawing = "?";
+  const drawing = { drawing: "picture" };
+  console.log("getting drawing?");
+  console.log("drawing", drawing);
+  //   socket.on("drawing", (data) =>
+  socket.broadcast.emit("drawing", drawing);
+  //   );
+};
 
 socketIo.on("connection", (socket) => {
-    console.log("New client connected")
-    if(interval) {
-        clearInterval(interval)
-    }
-    interval = setInterval(() => getApiAndEmit(socket), 1000)
-    socket.on("disconnect", () => {
-        console.log("Client disconnected")
-        clearInterval(interval)
-    })
-})
+  console.log("New client connected");
+  //   console.log("data", data);
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
+});
 
-const getApiAndEmit = socket => {
-    const response = new Date()
-    socket.emit("FromAPI", response)
-}
+socketIo.on("connection", (socket) => {
+  console.log("testing sending drawing");
+
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getDrawingAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("drawer disconnected");
+    clearInterval(interval);
+  });
+});
 
 server.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
+  console.log(`listening on port ${port}`);
+});
